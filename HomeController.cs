407da028b09c19a54e7e4823105128a54e31c5c2
Supreme Suwal <supremesuwal9@gmail.com>
2026-08-22 23:50:41 +0545

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -1,20 +1,25 @@
+using LoginSignup.Models;
 using Microsoft.AspNetCore.Mvc;
-using ModelBindingDemo.Models;
+using System.Diagnostics;
 
-namespace YourProjectName.Controllers
+namespace LoginSignup.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Index(Student student)
+        public IActionResult Privacy()
         {
-            return View("Result", student);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
